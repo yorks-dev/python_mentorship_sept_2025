0c5b4e506b00541e5980b02f0f522badd0b26fb0
Ayush Dutta <yorksdev@gmail.com>
2025-09-30 20:05:52 +0530

    Week 1 done

---
+++ week_1/doubts2.py
@@ -0,0 +1,18 @@
+# expression 1
+# a and not b or not c
+
+# Operator precedence : (), not , and , or
+
+# ex1 : (a and (not b)) or (not c) # similar
+# ex2 : (a and not b) or (not c)  # similar
+# ex3 : a and (not (b or not c))    # not similar
+# ex4 : a and ((not b) or (not c))   not similar
+
+
+# math expression -> 5 * 6 / 6 -> 5 * 1 -> 5
+# (), /, *, + , -
+
+# True and False or not False
+# -> True and False or True
+# -> False or True
+# -> True
